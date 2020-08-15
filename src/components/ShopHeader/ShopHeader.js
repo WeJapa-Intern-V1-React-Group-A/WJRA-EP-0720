39718b76e58e9b6413ca917d1be6f2d5059d7f02
TheAstutian <speakingatoms@gmail.com>
2020-08-15 19:22:40 +0100

    completed content for shopheader and shopnav

--- src/components/ShopHeader/ShopHeader.js
+++ src/components/ShopHeader/ShopHeader.js
@@ -1,15 +1,34 @@
 import React from 'react'
-import ShopNav from '../ShopNav/ShopNav'
+import ShopNav from '../ShopNav/ShopNav';
+import {FiMenu, FiSearch, FiChevronDown, FiHeart} from 'react-icons/fi';
+import {RiShoppingBagLine} from 'react-icons/ri';
 
 function ShopHeader() {
     return (
         <div className="shopheader-container">
             <header>
-            Header
+            <div>
+                <div>
+                 <FiMenu />
+                <FiSearch />
+                </div>
+                <div>
+                    IDEAL OF 9JA
+                </div>
+                <div>
+                    iPhone11 Pro <FiChevronDown />
+                    <FiHeart />
+                    <RiShoppingBagLine />
+                </div>
+            </div>
+            <div>
             <ShopNav/>
+            </div>
+                      
         </header>
         </div>
     )
 }
 
 export default ShopHeader
+
