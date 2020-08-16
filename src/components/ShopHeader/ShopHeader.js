389b13d8ff9b127ea6f1b6e3b1c185ff9ebf5639
TheAstutian <speakingatoms@gmail.com>
2020-08-16 22:19:06 +0100

    added styles for shopheader and shopnav components

--- src/components/ShopHeader/ShopHeader.js
+++ src/components/ShopHeader/ShopHeader.js
@@ -1,31 +1,52 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
+
 import ShopNav from '../ShopNav/ShopNav';
+
+
 import {FiMenu, FiSearch, FiChevronDown, FiHeart} from 'react-icons/fi';
 import {RiShoppingBagLine} from 'react-icons/ri';
 
-function ShopHeader() {
+import './shopheader.style.scss';
+
+const ShopHeader = () => {
     return (
-        <div className="shopheader-container">
-            <header>
-            <div>
-                <div>
-                 <FiMenu />
-                <FiSearch />
+        <div className="shopcontainer">
+            
+            <div className="shopheader-container">
+                <div className='logo-nav'>
+                    <Link className='logo'>
+                        <FiMenu size={22} />
+                    </Link>
+
+                    <Link className='logo'>
+                    <FiSearch size={22} />
+                    </Link>
                 </div>
-                <div>
-                    IDEAL OF 9JA
+
+                <div className="title-nav">
+                    <Link to='/'> <span> IDEAL OF 9JA </span> </Link>
                 </div>
-                <div>
-                   <p> iPhone11 Pro <FiChevronDown /></p>
-                    <FiHeart />
-                    <RiShoppingBagLine />
+
+                <div className="logo-nav"> 
+                  
+                  {(2===3) ? <div> SIGNOUT</div> :  (<div ><Link to='/signin'>
+                      <span > SIGN IN </span> </Link> 
+                      <Link to='/signup' > <span> REGISTER </span></Link></div>) }
+                    
+           
+                  <Link className='logo'>
+                    <FiHeart size={22}  />
+                  </Link>
+                    
+                    <Link className='logo'>
+                        <RiShoppingBagLine size={22} />
+                    </Link>
                 </div>
             </div>
-            <div>
-            <ShopNav/>
-            </div>
-                      
-        </header> 
+            <ShopNav />
+ 
         </div>
     )
 }
--- src/components/ShopNav/ShopNav.js
+++ src/components/ShopNav/ShopNav.js
@@ -1,26 +1,43 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
+import './shopnav.style.scss';
 
 function ShopNav() {
     return (
         <div className="shopnav-container">
-            <div className='for-her'>
-                for her
+            <div className='side-item'>
+               <Link> FOR HER</Link>
             </div>
-            <div className='navigation'>
-                <nav>
+            <div className='shop-navigation'>
                     <ul>
-                        <li>tops</li>
-                        <li>pants</li>
-                        <li>skirts</li>
-                        <li>outfits</li>
-                        <li>other wears</li>
-                        <li>accessories</li>
+                      <Link to='/categories/tops'> 
+                        <li>TOPS</li>
+                      </Link>  
+                      <Link to='/categories/trousers'>
+                        <li>PANTS</li>
+                      </Link>
+                      <Link to='/categories/skirts'>
+                        <li>SKIRTS</li>
+                      </Link>
+                      <Link to='/categories/outfits'>
+                        <li>OUTFITS</li>
+                      </Link>
+                      <Link to='/categories/otherwears'>
+                        <li>OTHER WEARS</li>
+                      </Link>
+                      <Link to='/categories/shoes'>
+                        <li>SHOES</li>
+                      </Link>
+                      <Link to='/categories/accessories'>
+                      <li>ACCESSORIES</li>
+                      </Link> 
+                    
                     </ul>
-            </nav>
             </div>
 
-            <div className='track-order'>
-                track order
+            <div className='side-item'>
+               <Link> TRACK ORDER</Link>
             </div>
         </div>
         
